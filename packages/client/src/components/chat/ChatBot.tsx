28ab0ed532945f4295cce479eb2bd9fe814f46a0
Ricsi <allili.hamid@gmail.com>
2025-09-03 21:22:59 +0200

    chore: Imports are corrected
    How this even worked before???
    
    refactor(backend): Chatbot component misplaced
    
    Oopsie :)

--- packages/client/src/components/chat/ChatBot.tsx
+++ packages/client/src/components/chat/ChatBot.tsx
@@ -1,42 +1,25 @@
-import ReactMarkDown from 'react-markdown';
-import { useEffect, useRef, useState, type KeyboardEvent } from 'react';
+import { useRef, useState } from 'react';
 import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import { FaArrowUp } from 'react-icons/fa';
-import { Button } from './ui/button';
-
-type FormData = {
-  prompt: string;
-};
+import TypingIndicator from './TypingIndicator';
+import type { Message } from './ChatMessages';
+import ChatMessages from './ChatMessages';
+import ChatInput, { type ChatFormData } from './ChatInput';
 
 type ChatResponse = {
   message: string;
-};
-
-type Message = {
-  content: string;
-  role: 'user' | 'bot';
 };
 
 const ChatBot = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [isBotTyping, setIsBotTyping] = useState(false);
   const [error, setError] = useState('');
-  const lastMessageRef = useRef<HTMLDivElement | null>(null);
   const conversationId = useRef(crypto.randomUUID());
-  const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
-  useEffect(() => {
-    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
-
-  const onSubmit = async ({ prompt }: FormData) => {
+  const onSubmit = async ({ prompt }: ChatFormData) => {
     try {
       setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
       setIsBotTyping(true);
       setError('');
-
-      reset({ prompt: '' });
 
       const { data } = await axios.post<ChatResponse>('/api/chat', {
         prompt,
@@ -51,57 +34,14 @@ const ChatBot = () => {
     }
   };
 
-  const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault();
-      handleSubmit(onSubmit)();
-    }
-  };
-
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
-        {messages.map((message, index) => (
-          <div
-            className={`px-3 py-1 rounded-xl ${
-              message.role === 'user'
-                ? 'bg-blue-600 text-white self-end'
-                : 'bg-gray-100 text-black self-start'
-            }`}
-            key={index}
-            ref={index === messages.length - 1 ? lastMessageRef : null}
-          >
-            <ReactMarkDown>{message.content}</ReactMarkDown>
-          </div>
-        ))}
-        {isBotTyping && (
-          <div className="flex gap-1 px-3 py-3 bg-gray-200 rounded-xl self-start">
-            <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse"></div>
-            <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.2s]"></div>
-            <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]"></div>
-          </div>
-        )}
+        <ChatMessages messages={messages} />
+        {isBotTyping && <TypingIndicator />}
         {error && <p className="text-red-500">{error}</p>}
       </div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        onKeyDown={onKeyDown}
-        className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-      >
-        <textarea
-          {...register('prompt', {
-            required: true,
-            validate: (data) => data.trim().length > 0,
-          })}
-          autoFocus
-          placeholder="Ask anything"
-          className="w-full border-0 focus:outline-0 resize-none"
-          maxLength={1000}
-        />
-        <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
-          <FaArrowUp />
-        </Button>
-      </form>
+      <ChatInput onSubmit={onSubmit} />
     </div>
   );
 };
